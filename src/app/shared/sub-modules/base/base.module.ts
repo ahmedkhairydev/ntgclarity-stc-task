c86371a69f307049606f8e8c0f10ce946507d9ee
Ahmed Khairy <=>
2023-08-05 13:01:10 +0300

    install bootstrap

--- src/app/shared/sub-modules/base/base.module.ts
+++ src/app/shared/sub-modules/base/base.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const MODULES = [
   RouterModule,
   TranslateModule,
+  FormsModule,
+  ReactiveFormsModule,
 ];
 
 @NgModule({
